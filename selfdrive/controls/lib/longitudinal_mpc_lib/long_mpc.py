74f3d1c2fd352bd4dae0809d243a3bb7e0c4aed5
cgw1968-5780 <156157950+cgw1968-5780@users.noreply.github.com>
2025-07-06 21:57:54 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -87,14 +87,14 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
 def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
   # The Dynamic follow function is adjusted by Marc(cgw1968-5779)
   if personality==log.LongitudinalPersonality.relaxed:
-    x_vel =  [0.,  6,   10., 10.01, 15., 27.7]
-    y_dist = [1.2, 1.4, 1.4,  1.5, 1.65,  1.8]
+    x_vel =  [0.,  2,   10., 27.7]
+    y_dist = [1.2, 1.3, 1.3, 1.75]
   elif personality==log.LongitudinalPersonality.standard:
-    x_vel =  [0.,  6,   10., 10.01, 15., 27.7]
-    y_dist = [1.1, 1.3, 1.35, 1.4,  1.4, 1.45]
+    x_vel =  [0.,  2,   10., 27.7]
+    y_dist = [1.1, 1.2, 1.2, 1.45]
   elif personality==log.LongitudinalPersonality.aggressive:
-    x_vel =  [0.,  6,   10., 10.01, 15., 27.7]
-    y_dist = [1.0, 1.2, 1.0,   0.9, 0.95, 1.0]
+    x_vel =  [0.,  2,   10., 27.7]
+    y_dist = [1.0, 1.1, 1.1, 1.25]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
